b4fcb0a531c34bd90c57def370165d7bbf81de6a
Budsabakorn Utanan <pppsoon3@gmail.com>
2022-08-27 22:09:30 +0700

    63_257_p6.3

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Forecast(props) {
     return (
-        <View >
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
+        <View  style={styles.div}>
+            <Text style={styles.main}>{props.main}</Text>
+            <Text >{props.description}</Text>
             <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <Text style={styles.temp}>°C</Text>
         </View>
     );
    }
+
+   const styles = StyleSheet.create({
+    div: {
+    flexDirexction: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+    },
+
+    main: {
+        marginTop: 10,
+        marginBottom: 20,
+        fontSize: 26,
+    },
+
+    temp: {
+        fontSize : 26,
+        fontWeight : 'bold',
+    }
+
+});
    
