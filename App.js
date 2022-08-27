91e889bc79509442fb8100b337bd0fc74c61fcfb
Budsabakorn Utanan <pppsoon3@gmail.com>
2022-08-27 17:19:27 +0700

    Part 2 : Hello world

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Wheater';
+import Constant from 'expo-constantc';
+ 
 
   export default function App() {
   const doIt = () => {
@@ -9,17 +12,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
   return (
     <View style={styles.container}>
-    <Text onPress={doIt}>Hello world</Text>
-    <StatusBar style="auto" />
+      <Weather zipCode="90110"/>
+      <StatusBar style="auto" />
  </View>
  );
 }
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      paddingTop: Constant.statusBarHeight
     },
 });
