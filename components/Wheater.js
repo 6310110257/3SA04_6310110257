99e54f31f001f98cb4de9f9b5d4cfe25edc6ddf9
Budsabakorn Utanan <pppsoon3@gmail.com>
2022-08-27 17:30:34 +0700

    Part 3 : Challenge

--- components/Wheater.js
+++ components/Wheater.js
@@ -11,7 +11,7 @@ export default function Weather(props) {
 
     return (
         <View>
-            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <ImageBackground source={require('../pic1.jpg')} style={styles.backdrop}>
                 <Text>Zip Code</Text>
                 <Text>{props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
@@ -22,8 +22,20 @@ export default function Weather(props) {
 
 const styles = StyleSheet.create({
     backdrop: {
+    flexDirection: 'column',
     alignItems: 'center',
     width: '100%',
     height: '100%'
     },
+    view: {
+        alignItems: 'center',
+        backgroundColor: 'black',
+        opacity: 0.5,
+        width: '100%',
+        height: '30%'
+    },
+    fonts: {
+        marginTop: 10,
+        fontSize: 20,
+    }
 });
